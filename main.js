99022db38f3e2cef9db067dc6cb08b99cb437e8a
riya2914 <riyapanchal2905@gmail.com>
2024-12-23 12:05:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var options = {
+        strings: ["Frontend Developer", "web Developer", "Freelancing"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    };
+    var typed = new Typed(".text", options);
+});
+
+
